dc1470b56bd01fdf982680666981185ae5416bfa
SiZiOUS <sizious@gmail.com>
2023-07-10 23:55:25 +0200

    Adding `EnumCom`: List all COM Ports See: https://github.com/sizious/windows-enumerate-serial-ports

---
+++ src/sys/embedded/enumcom/readme.txt
@@ -0,0 +1,5 @@
+# EnumCom
+
+Based on work done by Zach Gorman <gormanjz@hotmail.com>
+
+Source: https://github.com/sizious/windows-enumerate-serial-ports
---
+++ src/sys/enumcom.pas
@@ -0,0 +1,106 @@
+unit EnumCom;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TSerialPort = packed record
+    PortIndex: Integer;
+    DevicePath: string;
+    Name: string;
+    FriendlyName: string;
+    IsUSBDevice: Boolean;
+    Description: string;
+  end;
+
+  TSerialPortArray = array of TSerialPort;
+
+function EnumComPorts(var SerialPortArray: TSerialPortArray): Boolean;
+
+implementation
+
+{$R embedded/enumcom.rc}
+
+uses
+  SysTools,
+  FSTools,
+  RunTools,
+  Version,
+  CSVDocument;
+
+const
+  EMBEDDED_RESOURCE_ENUMCOM  = 'ENUMCOM';
+  EMBEDDED_FILENAME_ENUMCOM  = 'enumcom.exe';
+
+var
+  EnumComFileName: TFileName = '';
+
+function ParseResultFile(Buffer: string;
+  var SerialPortArray: TSerialPortArray): Boolean;
+var 
+  CSVDoc: TCSVDocument;
+  i: Integer;
+  SerialPort: TSerialPort;
+
+begin
+  Result := False;
+  CSVDoc := TCSVDocument.Create;
+  try
+    CSVDoc.Delimiter := '|';
+    CSVDoc.CSVText := Buffer;
+
+    // Allocate the required memory
+    SetLength(SerialPortArray, CSVDoc.RowCount - 1);
+
+    // Looping on all CSV rows extracted from EnumCom
+    for i := 1 to CSVDoc.RowCount - 1 do
+    begin
+      // Parsing the row
+      SerialPort.PortIndex := StrToInt(CSVDoc.Cells[0, i]);
+      SerialPort.DevicePath := CSVDoc.Cells[1, i];
+      SerialPort.Name := CSVDoc.Cells[2, i];
+      SerialPort.FriendlyName := CSVDoc.Cells[3, i];
+      SerialPort.IsUSBDevice := StrToBool(CSVDoc.Cells[4, i]);
+      SerialPort.Description := CSVDoc.Cells[5, i];
+
+      // Adding the COM Port to the final Array
+      SerialPortArray[i - 1] := SerialPort;
+    end;
+
+    Result := (High(SerialPortArray) + 1) = (CSVDoc.RowCount - 1);
+  finally
+    CSVDoc.Free;
+  end;
+end;
+
+function EnumComPorts(var SerialPortArray: TSerialPortArray): Boolean;
+var
+  OutputBuffer: string;
+  ProcessExitCode: Integer;
+
+begin
+  Result := False;
+  try
+    OutputBuffer := Default(string);
+    ProcessExitCode := Default(Integer);
+    if RunSingleCommand(EnumComFileName, OutputBuffer, ProcessExitCode) then
+      Result := ParseResultFile(OutputBuffer, SerialPortArray);
+  except
+    Result := False;
+  end;
+end;
+
+initialization
+  if not FileExists(EnumComFileName) then
+    EnumComFileName := ExtractEmbeddedFileToWorkingPath(
+      EMBEDDED_RESOURCE_ENUMCOM, EMBEDDED_FILENAME_ENUMCOM);
+
+finalization
+  KillFile(EnumComFileName);
+  
+end.
+
